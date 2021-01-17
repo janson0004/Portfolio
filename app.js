d64fff910a24fc9ffd0be1301223abefc03b36ee
janson_cheung <jansoncheung0004@gmail.com>
2021-01-18 01:39:48 +0800

    abut page with glider

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ hamburger.addEventListener("click", () => {
     list.classList.toggle("open");
     
 })
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    dots: '#dots',
+    draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+  });
